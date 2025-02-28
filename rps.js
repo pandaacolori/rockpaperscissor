c99d5477486b599d5a1b2c050d078c36f91eb31c
pandaacolori <federicocredaro@gmail.com>
2025-02-28 23:23:21 +0100

    the computer is playing alone

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
 console.log("Hello Chicco");
+let value1 = Math.random();
+console.log(value1);
+if (value1>=0.7) {
+    console.log("the computer played scissor");
+  }
+ else if (value1<=0.3) {
+    console.log("the computer played rock");
+  }
+else {
+    console.log("the computer played paper");
+}
